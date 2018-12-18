e75507dbc64389dcca607cd5146890a93f01279b
chunbong <cdy0266@gmail.com>
2018-12-18 19:02:47 +0900

    Add a problem solution

---
+++ Mathmatics/conversion_of_the_base.py
@@ -0,0 +1,13 @@
+n, b = map(int, input().strip().split(' '))
+answer = ""
+
+while ( n > 0 ):
+    r = n % b
+    if ( r < 10 ):
+        answer += str(r)
+    else:
+        tmp = r - 10 + ord('A')
+        answer += chr(tmp)
+    n //= b
+
+print(answer[::-1])

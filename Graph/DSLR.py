32de8d7bf335c201ba52686d27860620c43dec00
chunbong <cdy0266@gmail.com>
2019-07-27 09:34:24 +0900

    [수정] 결과 출력 방법 수정

--- Graph/DSLR.py
+++ Graph/DSLR.py
@@ -1,9 +1,12 @@
+# https://www.acmicpc.net/problem/9019
+
 from collections import deque
 
 
 def create_command(a, b):
     prev_num = [-1] * 10001
     how_made = [0] * 10001
+    # 각 지점까지 비용을 저장 & 방문했는지를 체크(-1이면 방문X)
     cost = [-1] * 10001
     queue = deque()
     queue.append(a)
@@ -12,24 +15,33 @@ def create_command(a, b):
 
     while queue:
         value = queue.popleft()
+
+        # D연산
         command_result = (value * 2) % 10000
         add_list(command_result, value, 'D', cost, how_made, prev_num, queue)
+
+        # S연산
         if value == 0:
             command_result = 9999
         else:
             command_result = value - 1
         add_list(command_result, value, 'S', cost, how_made, prev_num, queue)
+
+        # L연산
         command_result = (value % 1000)*10 + value//1000
         add_list(command_result, value, 'L', cost, how_made, prev_num, queue)
+
+        # R연산
         command_result = (value//10) + (value % 10)*1000
         add_list(command_result, value, 'R', cost, how_made, prev_num, queue)
 
+    answer = ""
     prev = prev_num[b]
-    print(how_made[b], end='')
+    answer += how_made[b]
     while prev != a:
-        print(how_made[prev], end='')
+        answer += how_made[prev]
         prev = prev_num[prev]
-    print()
+    print(answer[::-1])
 
 
 def add_list(command_result, value, command, cost, how_made, prev_num, queue):

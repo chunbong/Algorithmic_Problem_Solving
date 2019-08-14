e113e33365893959667081b6bb218d43b0b1ed84
chunbong <cdy0266@gmail.com>
2019-08-14 16:38:07 +0900

    수정

--- dynamic_programming/continuous_sum.py
+++ dynamic_programming/continuous_sum.py
@@ -6,7 +6,6 @@ answer[0] = seq[0]
 for i in range(1, n):
     including = answer[i-1] + seq[i]
     notIncluded = seq[i]
-    print(including, notIncluded)
     answer[i] = max(including, notIncluded)
 
 print(max(answer))

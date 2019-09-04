f77bf04cbeed1434ae9ce0247e9d17d90eb9edcd
chunbong <cdy0266@gmail.com>
2019-09-04 13:46:45 +0900

    [수정] 불필요한 코드 제거, 속도 개선

--- coding_test/KAKAO/실패율.py
+++ coding_test/KAKAO/실패율.py
@@ -1,44 +1,18 @@
-from operator import itemgetter
-
-
 def solution(N, stages):
-    answer = []
 
-    stages.sort()
-    l = len(stages)
-    stages.append(501)
-    now_stage = stages[0]
-    not_clear_user = 0
-    failure_rate = {}
-    check = [False] * N
-    i = 0
-    while True:
-        if stages[i] == now_stage:
-            not_clear_user += 1
+    usersPerStage = [0] * (N+1)
+    for stage in stages:
+        if stage == N+1:
+            continue
+        usersPerStage[stage] += 1
+
+    failureRate = {}
+    rechedUsers = len(stages)
+    for stage in range(1, N+1):
+        if rechedUsers != 0:
+            failureRate[stage] = usersPerStage[stage] / rechedUsers
+            rechedUsers -= usersPerStage[stage]
         else:
-            reched_user = l - i + not_clear_user
-            if now_stage != N+1:
-                failure_rate[now_stage] = float(
-                    not_clear_user) / float(reched_user)
-                check[now_stage-1] = True
-            now_stage = stages[i]
-            if now_stage == N+1:
-                break
-            not_clear_user = 1
+            failureRate[stage] = 0
 
-        if stages[i] == 501:
-            break
-
-        i += 1
-
-    temp = sorted(failure_rate.items(), key=itemgetter(0))
-    temp = sorted(failure_rate.items(), key=itemgetter(1), reverse=True)
-
-    for i in range(len(temp)):
-        answer.append(temp[i][0])
-
-    for i in range(N):
-        if check[i] == False:
-            answer.append(i+1)
-
-    return answer
+    return sorted(failureRate, key=lambda x: failureRate[x], reverse=True)

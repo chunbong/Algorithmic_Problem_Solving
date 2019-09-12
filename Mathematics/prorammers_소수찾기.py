eaf1e0168d6cc38ebdcbdeac2d3d388a3e6c672e
chunbong <cdy0266@gmail.com>
2019-09-12 22:24:53 +0900

    [추가] 문제해결 코드

---
+++ Mathematics/prorammers_소수찾기.py
@@ -0,0 +1,37 @@
+from itertools import permutations
+
+
+def findMax(numbers):
+    temp = sorted(numbers, reverse=True)
+    temp = ''.join(temp)
+    return int(temp)
+
+
+def solution(numbers):
+    answer = 0
+
+    splitNumbers = list(numbers)
+    maxNumber = findMax(numbers)
+
+    primeNumbers = [0] * (maxNumber + 1)
+    primeNumbers[0], primeNumbers[1] = -1, -1
+    for i in range(2, maxNumber+1):
+        j = i * i
+        while j <= maxNumber:
+            primeNumbers[j] = -1
+            j += i
+
+    check = [False] * (maxNumber + 1)
+    for number in numbers:
+        if check[int(number)] == False and primeNumbers[int(number)] == 0:
+            check[int(number)] = True
+            answer += 1
+
+    for length in range(2, len(numbers)+1):
+        permutationList = permutations(splitNumbers, length)
+        permutationList = set(permutationList)
+        for number in permutationList:
+            number = ''.join(number)
+            if check[int(number)] == False and primeNumbers[int(number)] == 0:
+                check[int(number)] = True
+                answer += 1

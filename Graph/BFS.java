c8c55bb2dbe3fdc8c420bcda466cfcdba54fc955
최다영 <dychoi.dev@gmail.com>
2021-04-03 12:33:50 +0900

    BFS 알고리즘 추가

---
+++ Graph/BFS.java
@@ -0,0 +1,29 @@
+package Graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+
+    public static int n = 10;
+    public static boolean[] visit = new boolean[n];
+    public static int[][] map = new int[n][n];
+
+    public static void bfs(int i) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(i);
+        visit[i] = true;
+
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+            System.out.print(temp + " ");
+
+            for (int j = 1; j < n+1; j++) {
+                if (map[temp][j] == 1 && !visit[j]) {
+                    q.offer(j);
+                    visit[j] = true;
+                }
+            }
+        }
+    }
+}

034c8ad3ffd84b5d43a1ae3ac50d62e3d7e437f9
최다영 <dychoi.dev@gmail.com>
2021-05-09 17:33:09 +0900

    HackerRank - Sales By Match

---
+++ Hackerrank_SalesByMatch.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Hackerrank_SalesByMatch {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+    public static int sockMerchant(int n, List<Integer> ar) {
+        int result = 0;
+
+        Map<Integer, Integer> pairMap = new HashMap<>();
+
+        for (Integer color : ar) {
+            if (pairMap.containsKey(color)) {
+                pairMap.put(color, pairMap.get(color) + 1);
+            } else {
+                pairMap.put(color, 1);
+            }
+        }
+
+        for (Integer color : pairMap.keySet()) {
+            int pair = pairMap.get(color);
+            if (pair >= 2) {
+                result += (pair / 2);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sockMerchant(15, Arrays.asList(6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5)));
+    }
+}

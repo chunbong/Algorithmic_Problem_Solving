534e7f74ac188e841249116f7bce951eb825cc6d
최다영 <dychoi.dev@gmail.com>
2021-06-27 13:06:03 +0900

    LeetCode - Valid Parentheses 풀이 추가

--- LeetCode_ValidParentheses.java
+++ LeetCode_ValidParentheses.java
@@ -1,12 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class LeetCode_ValidParentheses {
 
+    // ----- 내 풀이
     public static boolean isValid(String s) {
 
         Stack<Character> stack = new Stack<>();
 
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
             if (c == '(' || c == '{' || c == '[') {
@@ -32,6 +35,42 @@ public class LeetCode_ValidParentheses {
 
         return true;
     }
+    // -------
+
+    // ------- Leet Code Solution 1
+    private Map<Character, Character> mappings = new HashMap<>();
+
+    public LeetCode_ValidParentheses() {
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+
+    public boolean isValid_LeetCodeSolution1(String s) {
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (this.mappings.containsKey(c)) {
+                // 닫는 괄호를 만나면 pop
+
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                // 여는 괄호를 만나면 push
+                stack.push(c);
+            }
+
+        }
+
+        return stack.empty();
+    }
+    // -------
 
     public static void main(String[] args) {
         System.out.println(isValid("(]"));

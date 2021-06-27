df741409765dfdce4bbedcce4543585670b0e192
최다영 <dychoi.dev@gmail.com>
2021-06-27 12:37:25 +0900

    LeetCode - Longest Common Prefix 풀이 추가

--- LeetCode_LongestCommonPrefix.java
+++ LeetCode_LongestCommonPrefix.java
@@ -1,5 +1,6 @@
 public class LeetCode_LongestCommonPrefix {
 
+    // --- 내 풀이
     public static String longestCommonPrefix(String[] strs) {
 
         StringBuilder sb = new StringBuilder();
@@ -37,6 +38,22 @@ public class LeetCode_LongestCommonPrefix {
         }
 
         return sb.toString();
+    }
+    // ---
+
+    /**
+     * 코드 출처 - Leet Code
+     */
+    public static String longestCommonPrefix_verticalScanning(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length() ; i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j ++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
     }
 
     public static void main(String[] args) {
